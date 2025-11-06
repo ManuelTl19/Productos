33225cca6f8d005055d1406ed550edcf92ff7b0d
ManuelTl19 <20230147@utsh.edu.mx>
2025-11-05 23:32:46 -0600

    FrontEliminado

--- Frontend/src/app.js
+++
@@ -1,7 +0,0 @@
-import { router } from "./router.js";
-import { loadNavbar } from "./components/navbar.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-  loadNavbar();
-  router.navigate("/productos");
-});
